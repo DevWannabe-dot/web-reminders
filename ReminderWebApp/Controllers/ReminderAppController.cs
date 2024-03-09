aa1439cc2c2452cd4b223c423bc9fae607022993
Pedro H <a2022952558@teiacoltec.org>
2024-03-09 17:08:16 -0300

    Me rendi à chave na próxima linha... consertado problema de serialização do JSON

--- ReminderWebApp/Controllers/ReminderAppController.cs
+++ ReminderWebApp/Controllers/ReminderAppController.cs
@@ -2,37 +2,56 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
 using System.Data.SqlClient;
-using System.Security.AccessControl;
+using System.Collections.Generic;
 
-namespace ReminderWebApp.Controllers{
+namespace ReminderWebApp.Controllers
+{
     [Route("api/[controller]")]
     [ApiController]
-    public class ReminderAppController : ControllerBase{
-        private IConfiguration _configuration;
+    public class ReminderAppController : ControllerBase
+    {
+        private readonly IConfiguration _configuration; // não mudará uma vez inicializado
 
-        public ReminderAppController(IConfiguration configuration) {
+        public ReminderAppController(IConfiguration configuration)
+        {
             _configuration = configuration;
         }
 
         [HttpGet]
         [Route("GetReminders")]
-        public JsonResult GetReminders() {
-            String query = "SELECT * FROM reminders";
+        public IActionResult GetReminders()
+        {
+            string query = "SELECT * FROM dbo.reminders";
             DataTable table = new DataTable();
-            String SqlDatasource = _configuration.GetConnectionString("reminderAppDBConnection");
-            SqlDataReader myReader;
+            string sqlDatasource = _configuration.GetConnectionString("reminderAppDBConnection");
 
-            using (SqlConnection myCon = new SqlConnection(SqlDatasource)){
+            List<Dictionary<string, object>> remindersList = new List<Dictionary<string, object>>();
+
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
                 myCon.Open();
-                using (SqlCommand myCommand = new SqlCommand(query, myCon)){
-                    myReader = myCommand.ExecuteReader();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    SqlDataReader myReader = myCommand.ExecuteReader();
                     table.Load(myReader);
                     myReader.Close();
-                    // Bloco using vai automaticamente fechar myCon
+                    myCon.Close();
+                    // O bloco fecha myCommand ao final
                 }
+                // O bloco fecha myCon ao final
             }
 
-            return new JsonResult(table);
+            foreach (DataRow row in table.Rows)
+            {
+                var reminder = new Dictionary<string, object>();
+                foreach (DataColumn col in table.Columns)
+                {
+                    reminder[col.ColumnName] = row[col];
+                }
+                remindersList.Add(reminder);
+            }
+            /* O bloco de código acima recebe a tabela da base de dados, escreve cada linha em uma lista de dicionários e retorna na linha abaixo: */
+            return Ok(remindersList);   // Código 200 (OK)
         }
     }
 }

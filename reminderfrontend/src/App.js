c88afee962ad461743b751c352c41b0daf20fc8f
Pedro H <a2022952558@teiacoltec.org>
2024-03-10 22:55:38 -0300

    Tentamo

--- reminderfrontend/src/App.js
+++ reminderfrontend/src/App.js
@@ -1,25 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+class App extends Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props){
+    super(props);
+    this.state={
+      reminders:[]
+    }
+  }
+
+  API_URL="http://localhost:5066/";
+
+  componentDidMount(){
+    this.refreshReminders();
+  }
+
+  async refreshReminders(){
+    fetch(this.API_URL+"api/ReminderApp/Reminders").then(response=>response.json())
+    .then(data=>{
+      this.setState({reminders:data});
+    })
+  }
+
+  async addClick(){
+    var newReminder = document.getElementById("newReminder").value;
+    const data=new FormData();
+    data.append("newReminder", newReminder);
+
+    fetch(this.API_URL+"api/ReminderApp/Reminder",{
+      method:"POST",
+      body:data
+    }).then(res=>res.json())
+    .then((result)=>{
+      alert(result);
+      this.refreshReminders();
+    })
+  }
+
+  async deleteClick(id){
+    fetch(this.API_URL+"api/ReminderApp/Reminder?id="+id,{
+      method:"DELETE",
+    }).then(res=>res.json())
+    .then((result)=>{
+      alert(result);
+      this.refreshReminders();
+    })
+  }
+
+  render() {
+    const{reminders} = this.state;
+
+    return (
+      <div className="app">
+        <h2>Novo lembrete</h2>
+          <form classname="formStyle">
+            <span className="labelSpanStyle">
+            <label for="newReminder">Nome</label>
+            </span>
+            <input type="text" placeholder="Nome do lembrete" className="newReminder"/>&nbsp; <br/>
+          </form>
+          <form classname="formStyle">
+            <span className="labelSpanStyle">
+            <label for="newReminder">Data</label>
+            </span>
+            <input type="date" placeholder="Data do lembrete (no formato dd/mm/yyyy)" className="newReminder"/>&nbsp; <br/>
+          </form>
+
+          <button onClick={()=>this.addClick()} id="btnCriarStyle">Criar</button>
+        {reminders.map(reminder=>
+          <p>
+            {reminder.description}&nbsp;
+            <button onClick={()=>this.addClick(reminder.id)}>Delete Reminder</button>
+          </p>)}
+      </div>
+    );
+  }
 }
 
 export default App;
